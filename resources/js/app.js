1c8e05ba2879dc07eadd8296fdf263fba924e62f
Rupert Amodia <rupertraphael@live.com>
2018-09-18 14:54:02 -0600

    Style navbar
    Create UserDropdown component

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,8 @@ import './bootstrap';
 import vue from 'vue';
 window.Vue = vue;
 
+Vue.component('user-dropdown', require('./components/UserDropdown.vue'));
+
 const app = new Vue({
     el: '#app'
 });
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,42 +1,47 @@
-<nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-    <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">
-            {{ config('app.name', 'Laravel') }}
+<nav class="tw-fixed tw-w-full tw-border-b">
+    <div class="tw-px-12 tw-flex tw-justify-between tw-items-center">
+        <!-- Brand -->
+        <a class="tw-py-4 tw-text-xl tw-font-semibold" href="{{ url('/') }}">
+            {{ config('app.name') }}
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav mr-auto">
-
-            </ul>
-
+        <div>
             <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav ml-auto">
+            <ul class="tw-list-reset">
                 <!-- Authentication Links -->
                 @guest
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    <li>
+                        <a href="{{ route('login') }}">{{ __('Login') }}</a>
                     </li>
                 @else
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }} <span class="caret"></span>
-                        </a>
+                    <li>
+                        <user-dropdown inline-template>
+                            <div class="tw-text-sm">
+                                <a @click="active = !active" role="button">
+                                    {{ Auth::user()->name }}
+                                </a>
+                                <nav v-if="active" class="tw-mt-4 tw-absolute tw-bg-white tw-border">
+                                    <ul class="tw-list-reset">
+                                        <li class="tw-py-2 tw-pr-8 tw-pl-2">
+                                            <a>
+                                                Preferences
+                                            </a>
+                                        </li>
+                                        <li class="tw-py-2 tw-pr-4 tw-pl-2">
+                                            <a
+                                                href="{{ route('logout') }}"
+                                                onclick="event.preventDefault();
+                                                             document.getElementById('logout-form').submit();">
+                                                {{ __('Logout') }}
+                                            </a>
+                                        </li>
+                                    </ul>
 
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                                             document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
-                        </div>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="tw-hidden">
+                                        @csrf
+                                    </form>
+                                </nav>
+                            </div>
+                        </user-dropdown>
                     </li>
                 @endguest
             </ul>
