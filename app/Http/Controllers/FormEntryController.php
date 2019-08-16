2a06d6040f7cb88cbf33658c51bf1851175cb1a5
Rupert Amodia <rupert@hellokrd.net>
2019-08-16 16:59:14 -0600

    wip

--- app/Http/Controllers/FormEntryController.php
+++ app/Http/Controllers/FormEntryController.php
@@ -5,12 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Form;
+use App\FormEntry;
 use App\Http\Requests\StoreFormEntry;
 
 class FormEntryController extends Controller
 {
     public function store(Form $form, StoreFormEntry $request) 
     {
-    	return $request;
+    	$entry = new FormEntry;
+    	$entry->setTable($form->table_name);
+
+		$entry->create($request->validated());
+
+		return $entry->first();
     }
 }
